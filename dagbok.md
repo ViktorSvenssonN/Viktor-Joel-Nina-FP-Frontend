43e598d4496f4d4abf78f9e67fde2220ee2d7704
Mine Mine Mine <MasterNina@NBs-MacBook-Pro.local>
2023-01-06 23:31:46 +0100

    Fontawesome not working

--- dagbok.md
+++ dagbok.md
@@ -15,3 +15,10 @@ Dear dairy, what a day, let me tell you, it started off great with a team sessio
 - Routes för componenter
 -styling för Welcome page
 -styling för register
+
+6/1 Nina
+
+försökt få in fontawesom som iconer men fattar inte steget med babel set up config.
+https://fontawesome.com/v6/docs/web/use-with/react/add-icons
+"Add the following to your babel.config.js file:" hittar inte denna filen, måste jag skapa den själv?
+
--- src/components/ListvView.js
+++ src/components/ListvView.js
@@ -1,10 +1,12 @@
 import { InnerWrapper, OuterWrapper } from "Globalstyles";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import user from "reducers/user";
 import WithHeader from "./WithHeader";
 import styled from "styled-components/macro";
+import { icon } from "@fortawesome/fontawesome-svg-core";
 
 const ListView = () => {
   const dispatch = useDispatch();
@@ -22,6 +24,11 @@ const ListView = () => {
       <InnerWrapper>
         <h1>Welcome!</h1>
         <h2>Now you are logged in </h2>
+        <CrossBtn>✕
+        </CrossBtn>
+          <Icon>
+            <FontAwesomeIcon icon="fa-solid fa-check" size="3x"/>
+          </Icon>
       </InnerWrapper>
     </ListOuterWrapper>
   );
@@ -31,4 +38,41 @@ export default WithHeader(ListView);
 
 const ListOuterWrapper = styled(OuterWrapper)`
   background: var(--clr-background-light);
+`;
+
+
+// ------------- Styled Components -------------------
+
+export const CrossBtn = styled.button`
+  height: 40px;
+  width: 40px;
+  font-size: 1.5rem;
+  font-weight: bold;
+  border-radius: 25px;
+  border: none;
+  color: var(-clr-text-dark);
+  background-color: transparent;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:hover {
+  cursor: pointer;
+  background: var(-clr-text-dark);
+  color: var(-clr-text-light);
+  }
+
+  @media (min-width: 667px) {
+    
+  }
+  @media (min-width: 1024px) {
+    
+  }
+`;
+
+export const Icon = styled.div`
+border: 1px solid pink;
+width: 50px;
+height: 50px;
+
 `;
