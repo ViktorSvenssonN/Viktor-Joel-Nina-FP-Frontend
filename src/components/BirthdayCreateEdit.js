2c854014d23ccd293788b1ec3b63dc4a7cedf5c6
joelohman <joel.ohman@entryevent.se>
2023-01-08 15:39:16 +0100

    Adds functionality and styling to BirthdayCreateEdit

--- src/components/BirthdayCreateEdit.js
+++ src/components/BirthdayCreateEdit.js
@@ -1,6 +1,6 @@
 // Should be used for both creating new birthday, and edit, with props to change some labels
-import { InnerWrapper, OuterWrapper } from "Globalstyles";
-import React, { useEffect } from "react";
+import { OuterWrapper } from "Globalstyles";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 // import user from "reducers/user";
@@ -8,20 +8,33 @@ import styled from "styled-components/macro";
 import WithHeader from "./WithHeader";
 import closeIcon from "../images/icons/close.svg";
 import checkmarkIcon from "../images/icons/checkmark.svg";
+import { randomInt } from "./util";
 
 const BirthdayCreateEdit = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const location = useLocation();
+  const params = useParams();
 
+  const accessToken = useSelector((store) => store.user.accessToken);
+  const icons = useSelector((store) => store.ui.icons);
   const mode = location.pathname.includes("create") ? "create" : "edit";
 
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [icon, setIcon] = useState(null);
+
   // use the id to look up birthday from API/Redux
-  const params = useParams();
   const { id } = params;
 
-  console.log("mode:", mode);
+  useEffect(() => {
+    if (!accessToken) {
+      navigate("/login");
+    }
+  }, []);
 
   useEffect(() => {
-    if (location.pathname.includes("create")) {
+    if (mode === "create") {
       console.log("create");
     }
     if (mode === "edit") {
@@ -32,32 +45,55 @@ const BirthdayCreateEdit = () => {
     console.log(location);
   }, []);
 
-  const dispatch = useDispatch();
-  const accessToken = useSelector((store) => store.user.accessToken);
-  const icons = useSelector((store) => store.ui.icons);
-  const navigate = useNavigate();
-
-  console.log(icons);
-
   useEffect(() => {
-    if (!accessToken) {
-      navigate("/login");
-    }
+    setIcon(icons[randomInt(icons.length - 1)]);
   }, []);
+
+  const onFormCancel = () => {
+    const cancel = window.confirm("Are you sure you want to cancel?");
+    if (cancel) navigate("/home");
+  };
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    console.log("yay!");
+    // TODO
+    // push new birthday to API
+  };
 
   return (
     <ClonedOuterWrapper>
-      <ClonedInnerWrapper>
+      <FormWrapper onSubmit={onFormSubmit}>
         <BirthdayHeader>
-          <img src={closeIcon} alt="cancel" />
+          <HeaderButton type="button" onClick={onFormCancel}>
+            <img src={closeIcon} alt="cancel" />
+          </HeaderButton>
           <p>add birthday</p>
-          <img src={checkmarkIcon} alt="OK" />
+          <HeaderButton type="submit">
+            <img src={checkmarkIcon} alt="OK" />
+          </HeaderButton>
         </BirthdayHeader>
-        <TheRest>
-          <img src={icons[3]} />
-          <p>birthdayedit and shit yo!</p>
-        </TheRest>
-      </ClonedInnerWrapper>
+        <ContentWrapper>
+          <img src={icon} />
+          <TextInput
+            required
+            onChange={(event) => setFirstName(event.target.value)}
+            type="text"
+            placeholder="First name"
+            value={firstName}
+          />
+          <TextInput
+            required
+            onChange={(event) => setLastName(event.target.value)}
+            type="text"
+            placeholder="Last name"
+            value={lastName}
+          />
+          <BirthDateContainer>
+            <p>Birthdate</p>
+          </BirthDateContainer>
+        </ContentWrapper>
+      </FormWrapper>
     </ClonedOuterWrapper>
   );
 };
@@ -65,11 +101,10 @@ const BirthdayCreateEdit = () => {
 export default WithHeader(BirthdayCreateEdit);
 
 const ClonedOuterWrapper = styled(OuterWrapper)`
-  // css
   align-items: flex-end;
 `;
 
-const ClonedInnerWrapper = styled(InnerWrapper)`
+const FormWrapper = styled.form`
   width: 100%;
   height: 95%;
   display: flex;
@@ -77,6 +112,7 @@ const ClonedInnerWrapper = styled(InnerWrapper)`
 `;
 
 const BirthdayHeader = styled.header`
+  font-family: var(--font-second);
   display: flex;
   justify-content: space-around;
   align-items: center;
@@ -92,8 +128,49 @@ const BirthdayHeader = styled.header`
       brightness(94%) contrast(92%);
   }
 `;
+const HeaderButton = styled.button`
+  background: none;
+  border: none;
+`;
 
-const TheRest = styled.section`
+const ContentWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
   height: 90%;
+  padding: 30px;
   background: var(--clr-background-light);
+
+  img {
+    height: 100px;
+    width: 100px;
+    filter: invert(19%) sepia(8%) saturate(1926%) hue-rotate(195deg)
+      brightness(94%) contrast(92%);
+  }
+`;
+
+const TextInput = styled.input`
+  margin-top: 32px;
+  width: 75%;
+  font-family: inherit;
+  color: var(--clr-text-dark);
+  padding: 0px 0px 8px;
+  border: none;
+  outline: none;
+  font-size: 20px;
+  box-shadow: rgb(48 51 70 / 30%) 0px 1px;
+  background-color: transparent;
+
+  &:focus {
+    box-shadow: #303346 0px 2px;
+  }
+
+  &::placeholder {
+    opacity: 0.3;
+  }
+`;
+
+const BirthDateContainer = styled.section`
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
 `;
